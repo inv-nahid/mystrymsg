49fd08981d7f790e096be6b939e2fc2dc8d30cbf
inv-nahid <dripsage16@gmail.com>
2026-04-29 20:34:42 +0530

    add API route for verifying user account with verification code

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,65 @@
+import dbConnect from "@/lib/dbConnect"
+import UserModel from "@/models/user.model"
+import { z } from "zod"
+import { usernameValidation } from "@/schemas/signUp.schema"
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, code } = await request.json()
+    const decodedUsername = decodeURIComponent(username)
+    const user = await UserModel.findOne({ username: decodedUsername })
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 500 },
+      )
+    }
+
+    const isCodeValid = user.verifyCode === code
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date()
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true
+      await user.save()
+      return Response.json(
+        {
+          success: true,
+          message: "Account verified successfully",
+        },
+        { status: 200 },
+      )
+    }
+    else if (!isCodeNotExpired) {
+        return Response.json(
+      {
+        success: false,
+        message: "Verification code has expired! Please signup again to get a new code.",
+      },
+      { status: 400 },
+    )
+    }
+    else {
+        return Response.json(
+      {
+        success: false,
+        message: "Invalid verification code! Please check the code and try again.",
+      },
+      { status: 400 },
+    )
+    }
+
+  } catch (error) {
+    console.error("Error verifying code:", error)
+    return Response.json(
+      {
+        success: false,
+        message: "An error occurred while verifying code",
+      },
+      { status: 500 },
+    )
+  }
+}
